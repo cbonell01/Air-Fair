aaa29abdafa9749324c146f478c5c63620b220b7
Luke O'Sullivan <osulluke@gmail.com>
2021-10-04 17:57:07 -0700

    Add small html page.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const app = express()
 const PORT = 8080;
 
+// Show that the server responds to something.
 app.get('/', (req, res)=> {
     res.end('Hello World');
 })
 
+// Add a basic route
 app.get('/hello', (req, res) => {
     res.end('Here is the actual hello page');
 })
 
+// Add a routes homepage
+app.get('/routes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// Start the server and log to the console.
 app.listen(PORT, () => {
     console.log(`Express running on port ${PORT}`);
+    console.log(`Base directory: __dirname = ${__dirname}`);
 });

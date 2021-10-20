7860c27cf61b5a950d35fa6d477f907d36b00a08
cbonell01 <73505908+cbonell01@users.noreply.github.com>
2021-10-20 18:22:45 -0400

    Add styling and routes

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const path = require('path');
 const app = express()
 const PORT = 8080;
 
+app.use(
+    "/css",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+
 // Show that the server responds to something.
 app.get('/', (req, res)=> {
     res.end('Hello World');
@@ -17,6 +27,19 @@ app.get('/hello', (req, res) => {
 // Add a routes homepage
 app.get('/routes', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// About Us page
+app.get('/AboutUs', (req, res) => {
+    res.sendFile(path.join(__dirname, '/AboutUs.html'));
+});
+
+app.get('/Reports', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Reports.html'));
+});
+
+app.get('/Login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Login.html'));
 });
 
 // Start the server and log to the console.

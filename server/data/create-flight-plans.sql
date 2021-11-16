dacccfcf3138c1348cba12d7a7214c70a2b5c7d0
Luke O'Sullivan <osulluke@gmail.com>
2021-11-16 10:00:20 -0700

    Add some database file changes.

---
+++ server/data/create-flight-plans.sql
@@ -0,0 +1,6 @@
+CREATE TABLE flightplans( 
+    id              INTEGER     PRIMARY KEY,
+    origin          TEXT        NOT NULL,
+    destination     TEXT        NOT NULL,
+    flightplan      TEXT        NOT NULL
+);

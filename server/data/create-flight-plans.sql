3384a3277af09be299fff22238dcf93f993a78ef
Luke O'Sullivan <osulluke@gmail.com>
2021-11-16 21:54:42 -0700

    Update populate_flightplans.py
    Updates the population script; changes the DB script so it keeps the plan null if it's not ready to be loaded.

--- server/data/create-flight-plans.sql
+++ server/data/create-flight-plans.sql
@@ -4,5 +4,5 @@ CREATE TABLE flightplans(
     id              INTEGER     PRIMARY KEY,
     origin          TEXT        NOT NULL,
     destination     TEXT        NOT NULL,
-    flightplan      TEXT        NOT NULL
+    flightplan      TEXT        NULL
 );
--- server/data/populate_flightplans.py
+++ server/data/populate_flightplans.py
@@ -1,60 +1,65 @@
 import sqlite3, requests, itertools, json, time
+from requests.auth import HTTPBasicAuth
 
 # Connect to the database
 con = sqlite3.connect('fixes.db')
 cur = con.cursor()
-#results = cur.execute('select * from flightplans')
-#plans = results.fetchall()
-
-#for plan in plans:
-#    print(plan)
+results = cur.execute('select * from flightplans')
+plans = results.fetchall()
 
 # Set up flight plan API
 API_PLANS_ENPOINT = 'https://api.flightplandatabase.com/search/plans'
 API_ID_ENDPOINT = 'https://api.flightplandatabase.com/plan/'
-API_KEY = ''
+API_KEY = 'Xjinir5LX5SoqK7VwSczxmc6LpmKsy6JAdkxLbFL'
+API_KEY_2 = 'DDQAAVtKMjzyeNWQSfZLXMoCtbeNRb9h0kHl87lS'
 
 # Set up SQL queries
 insert_query = """INSERT INTO flightplans (origin, destination, flightplan) VALUES (?, ?, ?)"""
-#data = ('KSAN', 'KPHX', '{flightplan - KSAN to KPHX}')
+update_query = """UPDATE flightplans SET flightplan = ? WHERE id = ?"""
 
 # Create [origin, destination] permutations from list of airports
-airports = ['KSAN', 'KPHX', 'KLAX', 'KTUS', 'KSFO', 'KSEA', 'KSLC', 'KABQ', 'KDEN', 'KMSP', 'KDAL', 'KHOU', 'KMEM', 'KMSO', 'KJFK', 'KBOS', 'KPDX', 'KBOI', 'KBIL', 'KDIA', 'KCOS', 'KELP', 'KSAT', 'KDFW', 'KIAH', 'KMSY', 'KMDW', 'KORD', 'KMEM', 'KATL', 'KTPA', 'KMIA', 'KJAX', 'KSAV', 'KIAD', 'KDCA', 'KPHL', 'KLGA', 'KEWR', 'KBUF', 'KDTW', 'KCLE', 'KCVG', 'KLEX', 'KBHM', 'KCLT', 'KLAS', 'KOAK', 'KOMA', 'KOKC', 'KSTL', 'PHNL', 'KRNO', 'PANC']
-airports = set(airports)
-pairs = itertools.permutations(airports, 2)
+#airports = ['KSAN', 'KPHX', 'KLAX', 'KTUS', 'KSFO', 'KSEA', 'KSLC', 'KABQ', 'KDEN', 'KMSP', 'KDAL', 'KHOU', 'KMEM', 'KMSO', 'KJFK', 'KBOS', 'KPDX', 'KBOI', 'KBIL', 'KDIA', 'KCOS', 'KELP', 'KSAT', 'KDFW', 'KIAH', 'KMSY', 'KMDW', 'KORD', 'KMEM', 'KATL', 'KTPA', 'KMIA', 'KJAX', 'KSAV', 'KIAD', 'KDCA', 'KPHL', 'KLGA', 'KEWR', 'KBUF', 'KDTW', 'KCLE', 'KCVG', 'KLEX', 'KBHM', 'KCLT', 'KLAS', 'KOAK', 'KOMA', 'KOKC', 'KSTL', 'PHNL', 'KRNO', 'PANC']
+#airports = set(airports)
+#pairs = itertools.permutations(airports, 2)
 
 # Insert rows into database
 COUNT = 0
-for rte in pairs:
-    if COUNT > 1000:
-        break
+for rte in plans:
     COUNT += 1
+    row_ID = rte[0]
+    org = rte[1]
+    dest = rte[2]
+    route = rte[3]
+    database_route = None
+    AIRPORT_PARAMS = {'fromICAO': org, 'toICAO': dest}
+    #time.sleep(1.5)
 
-    AIRPORT_PARAMS = {'fromICAO': rte[0], 'toICAO': rte[1]}
-    print(AIRPORT_PARAMS)
-    time.sleep(5)
+    if route == None: # If there is no route for an [org, dest] pair, then get one
+        try:
+            # First, get a group of routes
+            response = requests.get(url = API_PLANS_ENPOINT, params=AIRPORT_PARAMS, auth=HTTPBasicAuth(API_KEY_2, ''))
+            print(response.text)
+            plan = json.loads(response.text)
+            plan_id = plan[0]['id']
+        except Exception:
+            print("Exception in GROUP")
 
-    try:
-        response = requests.get(url = API_PLANS_ENPOINT, params=AIRPORT_PARAMS)
-        print(response)
-        print(response.text)
-        plan = json.loads(response.text)
-        plan_id = plan[0]['id']
-        route = requests.get(url = API_ID_ENDPOINT + str(plan_id))
-        route_json = json.loads(route.text)
-        database_route = str(route_json['route']['nodes'])
-        data = (rte[0], rte[1], database_route)
-        cur.execute(insert_query, data)
-    #except Exception:
-        #print("Exception occurred.")
+        try:
+            # Second, get a specific route
+            route = requests.get(url = API_ID_ENDPOINT + str(plan_id), auth=HTTPBasicAuth(API_KEY_2, ''))
+            route_json = json.loads(route.text)
+            database_route = str(route_json['route']['nodes'])
+        except Exception:
+            print("Exception in SINGLE")
 
+        # Create the route for insertion into the database
+        data = (database_route, row_ID)
+
+        # Insert route into database
+        cur.execute(update_query, data)
         con.commit()
-    except Exception:
-        print(Exception)
 
-
-
-# Loop over [origin, destinations] to gather flight plans
-#cur.execute(insert_query, data)
+    #if COUNT > 1000:
+    #    break
 
 con.close()
